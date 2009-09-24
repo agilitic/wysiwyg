f215385ff8213cb422a191dad1f805ecbd60aa2f
aurels <aurels@gangsta.local>
2009-09-24 11:26:01 +0200

    fixes

--- generators/w_pictures/templates/models/wysiwyg_picture.rb
+++ generators/w_pictures/templates/models/wysiwyg_picture.rb
@@ -1,15 +1,14 @@
 class WysiwygPicture < ActiveRecord::Base
   
   has_attached_file :file,
-    :path => ":rails_root/public/system/w_pictures/:id/:style.:extension",
-    :url  => "/system/w_pictures/:id/:style.:extension",
-    :styles => { :medium => "300x300>",
+    :path => ":rails_root/public/system/wysiwyg_pictures/:id/:style.:extension",
+    :url  => "/system/wysiwyg_pictures/:id/:style.:extension",
+    :styles => {
       :thumb => "100x75>",
-      :slideshow => "300x200>"
-  }
+    }
 
   validates_attachment_presence     :file
-  validates_attachment_size         :file, :in => 1..5.megabyte
+  validates_attachment_size         :file, :in => 0..5.megabyte
   validates_attachment_content_type :file,
     :content_type => [ 'image/jpg', 'image/jpeg', 'image/jpe', 'image/png' ]
 end
